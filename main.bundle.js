c9408cf81c4e0ca6b5dc5f39ec5560927142a42d
Antoine Geboers <antoine@geboersjes.nl>
2017-01-23 22:42:37 +0100

    publish

--- main.bundle.js
+++ main.bundle.js
@@ -31641,9 +31641,13 @@ var _a;
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
 const environment = {
-    production: true,
-    serviceUrl: "http://247up-kmregistratie-service-prd.azurewebsites.net"
+    production: false,
+    serviceUrl: "http://247up-kmregistratie-service/"
 };
 /* harmony export (immutable) */ exports["a"] = environment;
 
